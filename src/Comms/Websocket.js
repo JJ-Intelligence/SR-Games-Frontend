d77780573ac45d5837b276da0c111107338e881c
Jack Parsons <jack.parsons.uk@icloud.com>
2021-08-19 22:46:48 +0100

    cors

--- src/Comms/Websocket.js
+++ src/Comms/Websocket.js
@@ -12,7 +12,9 @@ class Websocket{
 
 
 export function createLobby(callback) {
-    fetch("https://sr-games.herokuapp.com/https://sr-games-backend.herokuapp.com/createLobby")
+    fetch(
+        "https://sr-games.herokuapp.com/https://sr-games-backend.herokuapp.com/createLobby",
+        {mode: 'no-cors'})
         .then(e => e.text())
         .then(lobby => callback(lobby));
 }
