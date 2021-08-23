4e09636dc6eeef8c01d1ef106e554408985907c5
Josh Hook <joshhook@live.co.uk>
2021-08-23 19:39:41 +0100

    Fixed sendMessage

--- src/Comms/Websocket.js
+++ src/Comms/Websocket.js
@@ -9,11 +9,13 @@ export class WebsocketHandler {
         this.socket = new WebSocket('wss://sr-games-backend.herokuapp.com/');
         this.socket.onmessage = event => {console.log(event.data)}
         let sendMessage = this.sendMessage;
-        this.socket.onopen = () => sendMessage({
-            type: "LobbyJoinRequest", 
-            playerID: playerID,
-            lobbyID: lobbyID,
-        });
+        this.socket.onopen = () => sendMessage(
+            "LobbyJoinRequest",
+            {
+                playerID: playerID,
+                lobbyID: lobbyID,
+            }
+        );
 
         this.socket.addEventListener('message', event => {
             message = event.data
@@ -22,8 +24,11 @@ export class WebsocketHandler {
         });
     }
 
-    sendMessage(message) {
-        this.socket.send(JSON.stringify(message))
+    sendMessage(type, contents) {
+        this.socket.send(JSON.stringify({
+            type: type,
+            contents: contents
+        }))
     }
 
     addListener(type, callback) {
